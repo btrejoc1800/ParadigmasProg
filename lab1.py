a15992bea81f06d25b5eb62b5093b1666ddc4c1a
Daniel-M3na <alejand180815@gmail.com>
2023-03-31 12:29:06 -0600

    Python básico

---
+++ lab1.py
@@ -0,0 +1,13 @@
+"""list=[1,16,5,3216,546,513,216,54]
+
+if 10>5:
+    print("Que pedo perra xddd jajaja no se crea ")
+for i in list:
+    print(i)
+
+"""
+if 5>3:
+    print("fase 1")
+    if 5<321:
+        print ("Fase 2")
+
